37438e269944039f850205fbb316355733139a33
Rob Schoening <rschoening@lendingclub.com>
2014-02-15 11:36:03 -0800

    reformat

--- macgyver-test/src/main/java/io/macgyver/test/MacgyverIntegrationTest.java
+++ macgyver-test/src/main/java/io/macgyver/test/MacgyverIntegrationTest.java
@@ -4,7 +4,6 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
