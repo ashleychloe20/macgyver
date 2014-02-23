02726a725b39d25a219e7140d56643db2d902d36
Rob Schoening <rschoening@lendingclub.com>
2014-02-23 15:53:53 -0800

    additional capability

--- macgyver-ssh/src/main/java/io/macgyver/ssh/Ssh.java
+++ macgyver-ssh/src/main/java/io/macgyver/ssh/Ssh.java
@@ -1,10 +1,12 @@
 package io.macgyver.ssh;
 
 import java.io.IOException;
+import java.security.PublicKey;
 
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.connection.channel.direct.Session.Command;
+import net.schmizz.sshj.transport.verification.HostKeyVerifier;
 
 public class Ssh {
 
@@ -12,18 +14,21 @@ public class Ssh {
 	private String host;
 	private String command;
 
-	public static Ssh begin() {
-		return new Ssh();
-	}
+	private HostKeyVerifier verifier = null;
+	private int connectTimeout = -1;
 
 	public Ssh username(String username) {
 		this.username = username;
 		return this;
 	}
 
-	public Ssh host(String host) {
-		this.host = host;
-		return this;
+	public static Ssh to(String host) {
+		return hostname(host);
+	}
+	public static Ssh hostname(String host) {
+		Ssh ssh = new Ssh();
+		ssh.host = host;
+		return ssh;
 	}
 
 	public Ssh command(String command) {
@@ -31,15 +36,45 @@ public class Ssh {
 		return this;
 	}
 
-	public String execStringResponse() throws IOException {
+	public Iterable<String> exec() throws IOException {
+		return exec(new LineResponseCallback());
+	}
+
+	public String execString() throws IOException {
 		return exec(new StringResponseCallback());
+	}
+
+	public void connectTimeout(int timeout) {
+		this.connectTimeout = timeout;
+	}
+
+	public void hostKeyVerifier(HostKeyVerifier v) {
+		this.verifier = v;
+	}
+
+	public void withoutHostKeyVerification() {
+		verifier = new HostKeyVerifier() {
+
+			@Override
+			public boolean verify(String hostname, int port, PublicKey key) {
+				// TODO Auto-generated method stub
+				return true;
+			}
+		};
 	}
 
 	public <T> T exec(ResponseCallback<T> cb) throws IOException {
 
 		final SSHClient ssh = new SSHClient();
 		ssh.loadKnownHosts();
+
+		if (verifier != null) {
+			ssh.addHostKeyVerifier(verifier);
+		}
 		ssh.connect(host);
+		if (connectTimeout > 0) {
+			ssh.setConnectTimeout(connectTimeout);
+		}
 		try {
 			ssh.authPublickey(username);
 			final Session session = ssh.startSession();
