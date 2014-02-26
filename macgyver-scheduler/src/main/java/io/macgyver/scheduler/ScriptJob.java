76c52daad4b22f19d5caf1975042348b52bd8c39
Rob Schoening <rschoening@lendingclub.com>
2014-02-26 09:58:36 -0800

    fix #19 disallow concurrent ScriptJob execution

--- macgyver-scheduler/src/main/java/io/macgyver/scheduler/ScriptJob.java
+++ macgyver-scheduler/src/main/java/io/macgyver/scheduler/ScriptJob.java
@@ -4,12 +4,14 @@ import io.macgyver.core.script.ScriptExecutor;
 
 import java.io.File;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@DisallowConcurrentExecution
 public class ScriptJob implements Job {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
