2090dc46e2b7041a0e9eca63989ca0ab6d285a0a
Rob Schoening <robschoening@gmail.com>
2014-05-13 12:56:10 -0700

    add externalized script for testing

---
+++ macgyver-core/src/test/resources/ext/scripts/hooks/authBuilder.groovy
@@ -0,0 +1,9 @@
+import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider
+
+/*
+def activeDirectoryDomainName = "mydomain.com"
+def activeDirectoryLdapUrl    = "ldap://myserver.mydomain.com"
+def adProvider = new ActiveDirectoryLdapAuthenticationProvider(activeDirectoryDomainName, activeDirectoryLdapUrl)
+
+auth.authenticationProvider(adProvider)
+*/
