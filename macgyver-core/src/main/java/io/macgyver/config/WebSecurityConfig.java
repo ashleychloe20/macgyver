de2e67c02e10be2c628261ef872be44e3fc5e090
Rob Schoening <robschoening@gmail.com>
2014-05-14 06:47:08 -0700

    black arts of spring security

--- macgyver-core/src/main/java/io/macgyver/config/WebSecurityConfig.java
+++ macgyver-core/src/main/java/io/macgyver/config/WebSecurityConfig.java
@@ -1,17 +1,16 @@
 package io.macgyver.config;
 
-import java.util.Map;
-
 import io.macgyver.core.HookScriptManager;
 import io.macgyver.core.web.auth.InternalAuthenticationProvider;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
@@ -19,55 +18,72 @@ import com.google.common.collect.Maps;
 
 @Configuration
 @EnableWebMvcSecurity
-//@EnableWebSecurity
+// @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	InternalAuthenticationProvider internalAuthenticationProvider;
-	
+
 	@Autowired
 	HookScriptManager hookScriptManager;
-	
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-    	http.csrf().disable();
-    	
-    	AuthenticationManager x;
-    	
-    	
-        http
-            .authorizeRequests()
-                .antMatchers("/login","/resources/**", "/webjars/**").permitAll()
-                .antMatchers("/api/**").permitAll()
-                .antMatchers("/**").authenticated();
-        http
-            .formLogin()
-                .loginPage("/login").failureUrl("/login").defaultSuccessUrl("/")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();
-    }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Override
+	public void configure(WebSecurity web) throws Exception {
 
-    	auth.authenticationProvider(internalAuthenticationProvider);
-    	
-    	Map<String,Object> map = Maps.newHashMap();
-    	map.put("auth", auth);
-    	hookScriptManager.invokeHook("authBuilder",map);
-    }
-    
-    
-    
-    @Autowired
-    public void registerSharedAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-    	
-    	auth.authenticationProvider(internalAuthenticationProvider);
-    	
-    	Map<String,Object> map = Maps.newHashMap();
-    	map.put("auth", auth);
-    	hookScriptManager.invokeHook("authBuilder",map);
-    }
+		/*
+		 * Map<String, Object> map = Maps.newHashMap(); map.put("webSecurity",
+		 * web); hookScriptManager.invokeHook("configureWebSecurity", map);
+		 */
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable();
+
+		httpSecurity.authorizeRequests()
+
+		.antMatchers("/login", "/resources/**", "/webjars/**").permitAll().and().authorizeRequests()
+				.and().authorizeRequests().antMatchers("/**")
+				.authenticated().and().
+
+				formLogin().loginPage("/login").failureUrl("/login")
+				.defaultSuccessUrl("/").and().logout().permitAll().and().httpBasic();
+
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)
+			throws Exception {
+
+		auth.authenticationProvider(internalAuthenticationProvider);
+
+		Map<String, Object> map = Maps.newHashMap();
+		map.put("authBuilder", auth);
+		hookScriptManager.invokeHook("configureAuthProviders", map);
+	}
+
+	@Autowired
+	public void registerSharedAuthentication(AuthenticationManagerBuilder auth)
+			throws Exception {
+
+		auth.authenticationProvider(internalAuthenticationProvider);
+
+		Map<String, Object> map = Maps.newHashMap();
+		map.put("authBuilder", auth);
+		hookScriptManager.invokeHook("configureAuthProviders", map);
+	}
+	/*
+	  @Configuration
+	    @Order(20)
+	    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+	        // Since we didn't specify an AuthenticationManager for this class,
+	        // the global instance is used
+
+
+	        public void configure(HttpSecurity http) throws Exception {
+	          http.httpBasic().and().authorizeRequests().antMatchers("/api/**").authenticated();
+	        }
+	    }
+	*/
+
 }
--- macgyver-core/src/test/resources/ext/scripts/hooks/authBuilder.groovy
+++
@@ -1,9 +0,0 @@
-import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider
-
-/*
-def activeDirectoryDomainName = "mydomain.com"
-def activeDirectoryLdapUrl    = "ldap://myserver.mydomain.com"
-def adProvider = new ActiveDirectoryLdapAuthenticationProvider(activeDirectoryDomainName, activeDirectoryLdapUrl)
-
-auth.authenticationProvider(adProvider)
-*/
