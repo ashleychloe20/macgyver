cdf7310fc87811320bec783d574130438a3b0742
Rob Schoening <robschoening@gmail.com>
2014-05-17 15:16:09 -0700

    removed rogue import that was failing travis build

--- macgyver-core/src/main/java/io/macgyver/core/auth/GrantedAuthoritiesTranslator.java
+++ macgyver-core/src/main/java/io/macgyver/core/auth/GrantedAuthoritiesTranslator.java
@@ -2,16 +2,11 @@ package io.macgyver.core.auth;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Map;
-
-import javax.media.jai.operator.TranslateDescriptor;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 
 public  abstract class GrantedAuthoritiesTranslator implements GrantedAuthoritiesMapper {
 
