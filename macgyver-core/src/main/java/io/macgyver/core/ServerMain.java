4a8149f3cd203cc244ca7696566198941af898b7
Rob Schoening <robschoening@gmail.com>
2014-05-17 10:14:23 -0700

    #36 - customize banner

--- macgyver-core/src/main/java/io/macgyver/core/ServerMain.java
+++ macgyver-core/src/main/java/io/macgyver/core/ServerMain.java
@@ -1,5 +1,10 @@
 package io.macgyver.core;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Properties;
+
 import io.macgyver.core.mapdb.BootstrapMapDB;
 
 import org.mapdb.DB;
@@ -13,6 +18,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Strings;
+import com.google.common.io.CharStreams;
+import com.google.common.io.Files;
+
 /**
  * Simple wrapper to start server.
  * 
@@ -21,13 +31,16 @@ import org.springframework.core.env.Environment;
  */
 
 @Configuration
-@ComponentScan(basePackages={"io.macgyver.config","io.macgyver.plugin.config","io.macgyver.core.config"})
+@ComponentScan(basePackages = { "io.macgyver.config",
+		"io.macgyver.plugin.config", "io.macgyver.core.config" })
 @EnableAutoConfiguration
 public class ServerMain {
 
 	static Logger logger = org.slf4j.LoggerFactory.getLogger(ServerMain.class);
 
 	public static void main(String[] args) throws Exception {
+
+		printBanner();
 
 		BootstrapMapDB bootstrap = BootstrapMapDB.getInstance();
 		bootstrap.init();
@@ -40,12 +53,48 @@ public class ServerMain {
 			}
 		};
 		bootstrap.getTxMaker().get().execute(b);
+		SpringApplication app = new SpringApplication(ServerMain.class);
+		app.setShowBanner(false);
+		ConfigurableApplicationContext ctx = app.run(args);
 
-		ConfigurableApplicationContext ctx = SpringApplication.run(
-				ServerMain.class, args);
 		Environment env = ctx.getEnvironment();
 
 		logger.info("Spring environment: {}", env);
 
+	}
+
+	public static void printBanner() {
+
+		// Spring boot doesn't support alternate banner until 1.1.x
+		String bannerText = "\n";
+		try {
+			URL url = ServerMain.class.getResource("/banner.txt");
+			if (url != null) {
+				try (InputStreamReader reader = new InputStreamReader(
+						url.openStream(), Charsets.UTF_8)) {
+					String text = CharStreams.toString(reader);
+
+					bannerText += text;
+				}
+			}
+
+			url = null;
+			url = ServerMain.class
+					.getResource("/macgyver-core-revision.properties");
+			if (url != null) {
+				Properties p = new Properties();
+				try (InputStream x = url.openStream()) {
+					p.load(x);
+				}
+
+				bannerText += String.format("\n\n                      (v%s rev:%s)\n",
+								p.getProperty("version"),
+								p.getProperty("gitShortCommitId"));
+			}
+
+		} catch (Exception e) {
+			logger.warn("could not load banner");
+		}
+		System.out.println(bannerText);
 	}
 }
