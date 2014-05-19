c9a217353e42ea492c02630b58022a9522258b75
Rob Schoening <robschoening@gmail.com>
2014-05-19 13:17:55 -0700

    allow cert verification to be disabled

--- macgyver-elb-a10/src/main/java/io/macgyver/elb/a10/A10ClientServiceFactory.java
+++ macgyver-elb-a10/src/main/java/io/macgyver/elb/a10/A10ClientServiceFactory.java
@@ -15,10 +15,20 @@ public class A10ClientServiceFactory extends ServiceFactory<A10Client> {
 
 	@Override
 	protected A10Client doCreateInstance(ServiceDefinition def) {
-		
-		A10Client c = new A10Client(def.getProperties().getProperty("url"),def.getProperties().getProperty("username"),def.getProperties().getProperty("password"));
-		
-	
+
+		A10Client c = new A10Client(def.getProperties().getProperty("url"), def
+				.getProperties().getProperty("username"), def.getProperties()
+				.getProperty("password"));
+
+		try {
+			if (!Boolean.parseBoolean(def.getProperties().getProperty(
+					"certificateVerificationEnabled", "true"))) {
+				c.setCertificateVerificationEnabled(false);
+			}
+		} catch (Exception e) {
+			logger.warn("",e);
+		}
+
 		return c;
 	}
 
