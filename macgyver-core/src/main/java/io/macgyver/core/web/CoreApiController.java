b8dbe124234b04d5317858400acd9ac50e736684
Rob Schoening <robschoening@gmail.com>
2014-06-07 10:31:26 -0700

    add explicit GET

--- macgyver-core/src/main/java/io/macgyver/core/web/CoreApiController.java
+++ macgyver-core/src/main/java/io/macgyver/core/web/CoreApiController.java
@@ -14,6 +14,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.filter.DelegatingFilterProxy;
@@ -33,7 +34,7 @@ public class CoreApiController {
 	@Autowired
 	ExtensionResourceProvider extensionProvider;
 	
-	@RequestMapping(value = "/systemInfo", produces = { "application/json" })
+	@RequestMapping(value = "/systemInfo", method=RequestMethod.GET, produces = { "application/json" })
 	@ResponseBody
 	@PreAuthorize("permitAll")
 	public String coreRevisionInfo() {
