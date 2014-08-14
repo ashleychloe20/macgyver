3b41835b94038553dc576099f633934c9fcd1dc6
Rob Schoening <robschoening@gmail.com>
2014-08-13 23:28:29 -0700

    fix broken test

--- macgyver-plugin-cloud-aws/src/test/java/io/macgyver/plugin/cloud/aws/TestJClouds.java
+++ macgyver-plugin-cloud-aws/src/test/java/io/macgyver/plugin/cloud/aws/TestJClouds.java
@@ -15,6 +15,7 @@ import org.jclouds.domain.Location;
 import org.jclouds.ec2.EC2ApiMetadata;
 import org.jclouds.ec2.compute.EC2ComputeService;
 import org.jclouds.logging.slf4j.config.SLF4JLoggingModule;
+import org.junit.Assume;
 import org.junit.Test;
 import org.springframework.boot.logging.log4j.Log4JLoggingSystem;
 
@@ -25,29 +26,35 @@ public class TestJClouds extends MacGyverIntegrationTest {
 	@Test
 	public void testX() {
 
+		String awsAccessKey = getPrivateProperty("aws.accessKey");
+		String awsSecretKey = getPrivateProperty("aws.secretKey");
+
+		Assume.assumeTrue(awsAccessKey!=null);
+		Assume.assumeTrue(awsSecretKey!=null);
+		
 		Properties overrides = new Properties();
-	//	overrides.setProperty(Constants.PROPERTY_PROXY_HOST, "localhost");
-	//	overrides.setProperty(Constants.PROPERTY_PROXY_PORT, "8888");
+		// overrides.setProperty(Constants.PROPERTY_PROXY_HOST, "localhost");
+		// overrides.setProperty(Constants.PROPERTY_PROXY_PORT, "8888");
 		overrides.setProperty(Constants.PROPERTY_TRUST_ALL_CERTS, "true");
 		overrides.setProperty(Constants.PROPERTY_RELAX_HOSTNAME, "true");
-		overrides.setProperty("jclouds.regions","us-west-1,us-west-2,us-east-1");
-		ComputeServiceContext context = ContextBuilder
-				.newBuilder("aws-ec2").overrides(overrides)
-				.credentials(getPrivateProperty("aws.accessKey"),
-						getPrivateProperty("aws.secretKey"))
+		overrides.setProperty("jclouds.regions",
+				"us-west-1,us-west-2,us-east-1");
+		ComputeServiceContext context = ContextBuilder.newBuilder("aws-ec2")
+				.overrides(overrides).credentials(awsAccessKey, awsSecretKey)
 				.modules(ImmutableSet.of(new SLF4JLoggingModule()))
 				.buildView(ComputeServiceContext.class);
 
 		// http://jclouds.apache.org/guides/aws/
 		// https://github.com/jclouds/jclouds-examples/blob/master/compute-basics/src/main/java/org/jclouds/examples/compute/basics/MainApp.java
-		
-		  Set<? extends Location> locations =
-		  context.getComputeService().listAssignableLocations(); for (Location
-		  location: locations) { System.out.println("LOC: "+location); }
-		 
-		  
+
+		Set<? extends Location> locations = context.getComputeService()
+				.listAssignableLocations();
+		for (Location location : locations) {
+			System.out.println("LOC: " + location);
+		}
+
 		ComputeService svc = context.getComputeService();
-		
+
 		for (ComputeMetadata cn : svc.listNodes()) {
 			NodeMetadata nm = (NodeMetadata) cn;
 			System.out.println(nm.getId());
