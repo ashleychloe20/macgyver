4f43814d51d4347616e720f2e671e71b71b91297
Rob Schoening <robschoening@gmail.com>
2014-09-11 22:37:34 -0700

    add appInstance API

--- macgyver-plugin-cmdb/src/main/java/io/macgyver/plugin/cmdb/CmdbApiController.java
+++ macgyver-plugin-cmdb/src/main/java/io/macgyver/plugin/cmdb/CmdbApiController.java
@@ -1,6 +1,8 @@
 package io.macgyver.plugin.cmdb;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
@@ -9,8 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.MediaType;
 
 import io.macgyver.core.service.ServiceRegistry;
+import io.macgyver.neo4j.rest.Neo4jRestClient;
 import io.macgyver.xson.Xson;
 
+import org.ocpsoft.prettytime.PrettyTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -45,7 +50,10 @@ public class CmdbApiController {
 
 	ObjectMapper mapper = new ObjectMapper();
 	
-
+	@Autowired
+	Neo4jRestClient neo4j;
+	
+	
 
 	@RequestMapping(value = "checkIn", method = { RequestMethod.PUT,
 			RequestMethod.POST,RequestMethod.GET }, produces = "application/json")
@@ -69,5 +77,40 @@ public class CmdbApiController {
 		return new ResponseEntity<ObjectNode>(mapper.createObjectNode(), HttpStatus.OK);
 	}
 
+	@RequestMapping(value="/appInstances", method=RequestMethod.GET) 
+	@PreAuthorize("hasAnyRole('ROLE_MACGYVER_USER','ROLE_MACGYVER_API_RO')")
+	public ResponseEntity<List<ObjectNode>> allAppInstances() {
+		String cypher = "match (x:AppInstance) return x";
+		List<ObjectNode> results = neo4j.execCypher(cypher).asVertexDataList("x");	
+		beautifyTimestamps(results);
+		return new ResponseEntity<List<ObjectNode>>(results, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/appInstances/environment/{env}", method=RequestMethod.GET)
+	@PreAuthorize("hasAnyRole('ROLE_MACGYVER_USER','ROLE_MACGYVER_API_RO')")
+	public ResponseEntity<List<ObjectNode>> appInstancesByEnv(@PathVariable String env) {
+		String cypher = "match (x:AppInstance {environment:{env}}) return x";
+		List<ObjectNode> results = neo4j.execCypher(cypher,"env",env).asVertexDataList("x");
+		beautifyTimestamps(results);
+		return new ResponseEntity<List<ObjectNode>>(results, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/appInstances/environment/{env}/appId/{appId}", method=RequestMethod.GET) 
+	@PreAuthorize("hasAnyRole('ROLE_MACGYVER_USER','ROLE_MACGYVER_API_RO')")
+	public ResponseEntity<List<ObjectNode>> appInstance(@PathVariable String env, @PathVariable String appId) {
+		String cypher = "match (x:AppInstance {appId:{appIds},environment:{env}}) return x";
+		List<ObjectNode> results = neo4j.execCypher(cypher,"appIds",appId,"env",env).asVertexDataList("x");	
+		beautifyTimestamps(results);
+		return new ResponseEntity<List<ObjectNode>>(results, HttpStatus.OK);
+	}
+	
+	protected void beautifyTimestamps(List<ObjectNode> list) {
+		PrettyTime pt = new PrettyTime();
+		for (ObjectNode n : list) {
+			long val = n.path("lastContactTs").asLong(0);
+			Date d = new Date(val);
+			n.put("lastContactPrettyTs", pt.format(d));
+		}
+	}
 
 }
