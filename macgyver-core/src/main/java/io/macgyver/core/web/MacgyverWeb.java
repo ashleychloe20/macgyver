9f42bf3de94ee1b689769503e44c8d9ba5a929d3
Rob Schoening <robschoening@gmail.com>
2014-09-12 20:57:21 -0700

    #85 allow static web resources to be served from local dir

--- macgyver-core/src/main/java/io/macgyver/core/web/MacgyverWeb.java
+++ macgyver-core/src/main/java/io/macgyver/core/web/MacgyverWeb.java
@@ -1,5 +1,11 @@
 package io.macgyver.core.web;
 
+import java.io.File;
+import java.net.MalformedURLException;
+
+import io.macgyver.core.Bootstrap;
+import io.macgyver.core.MacGyverConfigurationException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,13 +14,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class MacgyverWeb extends WebMvcConfigurerAdapter {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-		logger.info("registering resources");
-		registry.addResourceHandler("/resources/**").addResourceLocations(
-				"classpath:/web/resources/");
+		try {
+			String webDir = new File(Bootstrap.getInstance().getWebDir(),
+					"resources").getAbsoluteFile().toURI().toURL().toString();
+			String classpathLocation = "classpath:/web/resources/";
+			logger.info("static resources served from: {} and {}", webDir,
+					classpathLocation);
+			registry.addResourceHandler("/resources/**").addResourceLocations(
+					webDir, classpathLocation);
+		} catch (MalformedURLException e) {
+			throw new MacGyverConfigurationException(e);
+		}
 	}
-	
-	
+
 }
